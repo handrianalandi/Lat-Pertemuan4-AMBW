9c99aed8cab8847c03a8e84f43e0b65bcebcb2b1
handrianalandi <handrianalandi@users.noreply.github.com>
2022-03-05 00:55:21 +0700

    coding upload

--- bin/latpertemuan4c14190231.dart
+++ bin/latpertemuan4c14190231.dart
@@ -1,3 +1,59 @@
+import 'dart:io';
+
+//asumsi 2 digit ditulis angka belakangnya 10=0, 11=1, 12=2, 13=3, dst.
+//c14190231
+void draw(int n) {
+  if (n % 2 != 0) {
+    int cstar = (n / 2).floor();
+    int cnum = 1;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < cstar; j++) {
+        stdout.write("*");
+      }
+      for (int j = 0; j < cnum; j++) {
+        if (j % 2 != 0) {
+          stdout.write(" ");
+        } else {
+          stdout.write((i + 1) % 10);
+        }
+      }
+      for (int j = 0; j < cstar; j++) {
+        stdout.write("*");
+      }
+      if (i < (n / 2).floor()) {
+        cstar--;
+        cnum += 2;
+      } else {
+        cstar++;
+        cnum -= 2;
+      }
+      print("");
+    }
+  } else {
+    int kiri = 1, kanan = n;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - i; j++) {
+        stdout.write(kiri++ % 10);
+        if (kiri > n) {
+          kiri = 1;
+        }
+      }
+      for (int j = 0; j < i * 2; j++) {
+        stdout.write(" ");
+      }
+      for (int j = 0; j < n - i; j++) {
+        stdout.write(kanan-- % 10);
+        if (kanan < 1) {
+          kanan = n;
+        }
+      }
+      print("");
+    }
+  }
+}
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  stdout.write("n: ");
+  String? n = stdin.readLineSync();
+  draw(int.parse(n!));
 }
